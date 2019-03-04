3c3a3a9c777750adaebcb5b025558484a13310e3
XcJimsun <1543510543@qq.com>
2019-03-04 21:36:45 +0800

    编写堆排序和测试

---
+++ src/main/java/com/jimsun/implement/heapSort.java
@@ -0,0 +1,43 @@
+package com.jimsun.implement;
+
+public class heapSort<T extends Comparable<T>> extends sortabstract<T> {
+
+	@Override
+	public void sortDesc() {
+		// TODO Auto-generated method stub
+		T[] arr = getElement();
+		int length = arr.length;	
+		for(int i=length-1;i>0;i--) {
+			buildHeap(arr,0,i);
+			swap(arr,0,i);
+		}
+	}
+	private void buildHeap(T[] arr,int start,int end) {
+		for(int i = (end+1)/2;i>=start;i--) {
+			ajdustHeap(arr,i,end+1);
+		}
+	}
+	private void ajdustHeap(T[] arr,int parent,int length) {
+		T temp = arr[parent];
+		for(int i=2*parent+1;i<length;i=2*i+1) {
+			if(i+1<length && arr[i].compareTo(arr[i+1])<0) {
+				++i;
+			}
+			if(arr[i].compareTo(temp)<0) {
+				break;
+			}else {
+				arr[parent] = arr[i];
+				parent = i;
+				//swap(arr,a,i);
+			}
+		}
+		arr[parent] = temp;
+	}
+	
+	@Override
+	public void sortAsc() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/test/java/com/jimsun/sort/TestUntil.java
+++ src/test/java/com/jimsun/sort/TestUntil.java
@@ -3,6 +3,7 @@ package com.jimsun.sort;
 import org.junit.Test;
 
 import com.jimsun.implement.Sort;
+import com.jimsun.implement.heapSort;
 import com.jimsun.implement.quikSort;
 
 public class TestUntil {
@@ -17,8 +18,9 @@ public class TestUntil {
 	public void test0(){
 		//Sort<Integer> test0=new directInsert<Integer>();
 		//Sort<Integer> test0=new shellSort<Integer>();
-		//Sort<Integer> test0=new simplechooseSort<Integer>();
-		Sort<Integer> test0=new quikSort<Integer>();
+		//Sort<Integer> test0=new simplechooseSort<Integ
+		//Sort<Integer> test0=new quikSort<Integer>();
+		Sort<Integer> test0=new heapSort<Integer>();
 		test0.setElement(arr);
 		test0.sortAndprint(true);
 	}
